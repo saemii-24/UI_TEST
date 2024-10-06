99473c06d4ce85dbe8e6433dc034734fe8498a82
saemii-24 <hims2419@gmail.com>
2024-10-06 18:32:28 +0900

    Feat: API 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Provider from "./Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  return <div></div>;
+  // 환경 변수 불러오기
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const fetchURL = `https://pixabay.com/api/?key=${apiKey}&q=yellow+flowers&image_type=photo`;
+
+  const fetchData = async () => {
+    const response = await fetch(fetchURL);
+    if (!response.ok) {
+      throw new Error("응답 도중 오류가 발생했습니다.");
+    }
+    return response.json();
+  };
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["searchPicture"],
+    queryFn: fetchData,
+  });
+
+  console.log(data);
+
+  if (isLoading) return <div>로딩중</div>;
+  return (
+    <div>
+      <button className='bg-red-400 px-5 py-2'>이미지 불러오기</button>
+    </div>
+  );
 }
