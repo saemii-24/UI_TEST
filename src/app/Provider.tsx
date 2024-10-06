bdded25c326bc31779e277b4cf69f5fb2bba363f
saemii-24 <hims2419@gmail.com>
2024-10-06 18:31:41 +0900

    Feat: api key

---
+++ src/app/Provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient();
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default Provider;
