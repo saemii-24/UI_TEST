2c06e185f430bf402dcdea4310a00a35074d4522
saemii-24 <hims2419@gmail.com>
2024-10-10 17:37:23 +0900

    feat: vitest setting

---
+++ src/test/Dropdown.spec.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import Dropdown from "@/components/Dropdown";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("Dropdown 컴포넌트 테스트", () => {
+  it("사용자가 input을 한 글자이상 작성하면 Dropdown이 표시되어야 한다.", () => {});
+  it("Dropdown의 close 아이콘을 선택한 경우 Dropdown에서 사라지며, localStorage에서도 사라져야 한다.", () => {});
+});
