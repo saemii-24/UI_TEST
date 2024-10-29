76fcb993750dd35479da2af3d274214db11691a4
saemii-24 <hims2419@gmail.com>
2024-10-29 19:21:03 +0900

    Test: fetch test list

--- src/test/Fetch.spec.tsx
+++ src/test/Fetch.spec.tsx
@@ -9,4 +9,16 @@ describe("ImageCard 테스트", () => {
     const expected = mockImageData;
     expect(data).toEqual(expected);
   });
+
+  it("유저 정보가 있을 때 'Photo by. [user]' 텍스트를 렌더링한다.", async () => {});
+
+  it("이미지 URL이 없을 때 컴포넌트가 렌더링되지 않는다.", () => {});
+
+  it("이미지의 src와 alt 속성이 전달된 데이터에 맞게 설정된다.", () => {});
+
+  it("user와 userImageURL이 없을 경우 사용자 정보가 렌더링되지 않는다.", () => {});
+
+  it("이미지 컨테이너에 마우스를 올렸을 때 스타일이 변경된다.", () => {});
+
+  it("접근성을 위해 alt 텍스트가 이미지 ID와 사용자 정보를 포함한다.", () => {});
 });
