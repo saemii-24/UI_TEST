39cf65a49b2470e2c277f6274bd86dbce18f3372
saemii-24 <hims2419@gmail.com>
2024-11-05 20:51:32 +0900

    Temp: blurDataUrl

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import type { ImageListProps } from "@/types/type";
+import getBase64 from "@/utils";
+import { mockImageData } from "@/test/mockdata";
 
-const ImageCard = ({ imageList }: { imageList: Partial<ImageListProps> }) => {
+const ImageCard = async ({
+  imageList = mockImageData[0],
+}: {
+  imageList: Partial<ImageListProps>;
+}) => {
   if (!imageList.webformatURL) return;
+  if (!imageList.previewURL) return;
+
+  const myBlurDataUrl = await getBase64(imageList.previewURL);
 
   return (
     <div data-testid='image-card'>
       <div className='relative w-full max-w-[400px] cursor-pointer overflow-hidden rounded-xl before:absolute before:z-10 before:size-full before:opacity-40 before:transition-all before:duration-150 hover:before:bg-black'>
         <Image
           src={imageList.webformatURL}
+          blurDataURL={myBlurDataUrl}
+          placeholder='blur'
           alt={imageList.id + "image-thumbnail" + imageList.user}
           width={0}
           height={0}
