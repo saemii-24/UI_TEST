4d49e04b9dbaaf1405821f3d776ce608a8898054
saemii-24 <hims2419@gmail.com>
2024-11-06 19:07:38 +0900

    Feat: image size

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import type { ImageListProps } from "@/types/type";
-import getBase64 from "@/utils";
 import { mockImageData } from "@/test/mockdata";
 
-const ImageCard = async ({
+const ImageCard = ({
   imageList = mockImageData[0],
 }: {
   imageList: Partial<ImageListProps>;
 }) => {
-  if (!imageList.webformatURL) return;
-  if (!imageList.previewURL) return;
+  const imageCardRef = useRef<HTMLDivElement>(null);
+  const [cardWidth, setCardWidth] = useState(0);
 
-  const myBlurDataUrl = await getBase64(imageList.previewURL);
+  // 이미지 비율에 맞는 height 계산
+  const calculateHeight = (width: number) => {
+    return (width * (imageList.imageHeight ?? 1)) / (imageList.imageWidth ?? 1);
+  };
+
+  useEffect(() => {
+    if (imageCardRef.current) {
+      setCardWidth(imageCardRef.current.offsetWidth);
+    }
+    // 리사이즈 이벤트에 대응
+    const handleResize = () => {
+      if (imageCardRef.current) {
+        setCardWidth(imageCardRef.current.offsetWidth);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!imageList.webformatURL) return null;
 
   return (
-    <div data-testid='image-card'>
+    <div data-testid='image-card' ref={imageCardRef}>
       <div className='relative w-full max-w-[400px] cursor-pointer overflow-hidden rounded-xl before:absolute before:z-10 before:size-full before:opacity-40 before:transition-all before:duration-150 hover:before:bg-black'>
         <Image
+          className=' bg-gray-200'
           src={imageList.webformatURL}
-          blurDataURL={myBlurDataUrl}
-          placeholder='blur'
-          alt={imageList.id + "image-thumbnail" + imageList.user}
-          width={0}
-          height={0}
-          sizes='100vw'
-          style={{ width: "100%", height: "auto" }}
+          alt={`${imageList.id} image-thumbnail ${imageList.user}`}
+          width={cardWidth}
+          height={calculateHeight(cardWidth)}
+          sizes={`${cardWidth}px`}
           priority
         />
       </div>
@@ -33,11 +49,11 @@ const ImageCard = async ({
         imageList.user.length > 0 &&
         imageList.userImageURL &&
         imageList.userImageURL.length > 0 && (
-          <div className='mt-2 flex items-center  gap-2'>
+          <div className='mt-2 flex items-center gap-2'>
             <div className='relative size-10 overflow-hidden rounded-full'>
               <Image
                 src={imageList.userImageURL}
-                alt={imageList.id + "image-user" + imageList.user}
+                alt={`${imageList.id} image-user ${imageList.user}`}
                 fill
                 style={{ objectFit: "cover" }}
                 priority
--- src/components/PageError.tsx
+++ src/components/PageError.tsx
@@ -4,14 +4,16 @@ import { BiError } from "react-icons/bi";
 
 const Error = () => {
   return (
-    <div
-      className={cn(
-        "flex w-full flex-col items-center justify-center gap-4 text-red-500",
-      )}
-    >
-      <BiError className='text-5xl ' />
-      <div className='font-medium  text-red-500'>
-        데이터를 불러오는 중 오류가 발생했습니다.
+    <div className='flex h-screen w-full items-center justify-center'>
+      <div
+        className={cn(
+          "flex w-full flex-col items-center justify-center gap-4 text-red-500",
+        )}
+      >
+        <BiError className='text-5xl ' />
+        <div className='font-medium  text-red-500'>
+          데이터를 불러오는 중 오류가 발생했습니다.
+        </div>
       </div>
     </div>
   );
