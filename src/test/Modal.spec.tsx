863f64551b099f98465524b86a40d9ee5a1e7a1e
saemii-24 <hims2419@gmail.com>
2024-11-12 19:32:06 +0900

    Temp: modal test list

--- src/test/Modal.spec.tsx
+++ src/test/Modal.spec.tsx
@@ -4,6 +4,7 @@ import { mockImageData } from "./mockdata";
 import { create } from "zustand";
 import { ImageListProps } from "@/types/type";
 import { ModalImageId, useModalImageId } from "@/store/mockModalImageIdStore";
+import ImageCard from "@/components/ImageCard";
 
 // Zustand 스토어 초기화 및 리셋 함수 설정
 const storeResetFns = new Set<() => void>();
@@ -28,22 +29,24 @@ beforeEach(() => {
 });
 
 describe("Modal 컴포넌트 테스트", () => {
-  it("사용자가 모달 Close 버튼을 클릭할 때 모달이 닫힌다.", () => {});
-
-  it("ImageCard 클릭 시 Modal이 표시되어야 한다", async () => {});
-
-  it("zustand 최조는 mockImageData[0]이다.", () => {
+  it("mocking된 zustand 최초 데이터는 mockImageData[0]이다.", () => {
     const store = useModalImageId.getState();
     expect(store.modalImage).toEqual(mockImageData[0]);
   });
 
-  it("사용자가 ESC키를 눌렀을 때 모달이 닫힌다.", () => {});
+  it("ImageCard 클릭 시 Modal이 표시되어야 한다", async () => {
+    const { getByTestId } = render(<ImageCard imageList={mockImageData[0]} />);
+  });
 
-  it("모달 외부를 클릭 시 모달이 닫히는지 확인한다", () => {});
+  // it("사용자가 모달 Close 버튼을 클릭할 때 모달이 닫힌다.", () => {});
 
-  it("사용자가 좋아요를 눌렀을 때 fill 하트 아이콘으로 변경된다.", async () => {});
+  // it("사용자가 ESC키를 눌렀을 때 모달이 닫힌다.", () => {});
 
-  it("사용자가 다운로드 버튼을 클릭하면 해당 url의 새로운 창이 열린다.", () => {});
+  // it("모달 외부를 클릭 시 모달이 닫히는지 확인한다", () => {});
 
-  it("사용자가 공유 버튼을 클릭하면 해당 url이 복사된다.", async () => {});
+  // it("사용자가 좋아요를 눌렀을 때 fill 하트 아이콘으로 변경된다.", async () => {});
+
+  // it("사용자가 다운로드 버튼을 클릭하면 해당 url의 새로운 창이 열린다.", () => {});
+
+  // it("사용자가 공유 버튼을 클릭하면 해당 url이 복사된다.", async () => {});
 });
