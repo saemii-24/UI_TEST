dd01e77387f3944007ab0a41c9abaf388b04a0b2
saemii-24 <hims2419@gmail.com>
2024-11-25 17:30:27 +0900

    Chore: babel, next config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  plugins: ["@babel/plugin-transform-private-methods"],
-  presets: ["@babel/preset-react", "@babel/preset-flow"],
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+    "@babel/preset-react", // JSX와 관련된 설정을 위한 프리셋 추가
+    "next/babel",
+  ],
+  plugins: [
+    "@babel/plugin-transform-react-jsx", // React 17 이상에서는 이 플러그인 추가
+  ],
 };
